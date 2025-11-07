81ada84aa235fa148be8bb6050384ffa2e3a3a3e
Limit <60055811+SxyLimit@users.noreply.github.com>
2025-11-07 22:28:19 +0800

    Improve prompt width calculation across platforms

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#ifdef _WIN32
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#else
 #include <termios.h>
 #include <unistd.h>
 #include <poll.h>
+#endif
 #include <cerrno>
 #include <csignal>
 #include <cstdio>
@@ -9,6 +15,7 @@
 #include <clocale>
 #include <codecvt>
 #include <cwchar>
+#include <fstream>
 #include <locale>
 #include <stdexcept>
 #include <cctype>
@@ -28,6 +35,154 @@
 #include "globals.hpp"
 #include "tools.hpp"
 #include "settings.hpp"
+
+namespace platform {
+#ifdef _WIN32
+
+class TermRaw {
+  HANDLE hIn = INVALID_HANDLE_VALUE;
+  DWORD origMode = 0;
+  bool active = false;
+public:
+  void enable(){
+    hIn = GetStdHandle(STD_INPUT_HANDLE);
+    if(hIn == INVALID_HANDLE_VALUE) std::exit(1);
+    if(!GetConsoleMode(hIn, &origMode)) std::exit(1);
+    DWORD mode = origMode;
+    mode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT);
+    mode |= ENABLE_VIRTUAL_TERMINAL_INPUT;
+    if(!SetConsoleMode(hIn, mode)) std::exit(1);
+    active = true;
+  }
+  void disable(){
+    if(active){
+      SetConsoleMode(hIn, origMode);
+      active = false;
+    }
+  }
+  ~TermRaw(){ disable(); }
+};
+
+inline void ensure_virtual_terminal_output(){
+  static bool initialized = false;
+  if(initialized) return;
+  initialized = true;
+
+  // Ensure the console understands UTF-8 sequences.
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+  _setmode(_fileno(stdout), _O_BINARY);
+  _setmode(_fileno(stderr), _O_BINARY);
+  _setmode(_fileno(stdin), _O_BINARY);
+
+  auto enable_virtual_terminal = [](DWORD stdHandle){
+    HANDLE handle = GetStdHandle(stdHandle);
+    if(handle == INVALID_HANDLE_VALUE) return;
+    DWORD mode = 0;
+    if(!GetConsoleMode(handle, &mode)) return;
+    mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    mode |= DISABLE_NEWLINE_AUTO_RETURN;
+    SetConsoleMode(handle, mode);
+  };
+
+  enable_virtual_terminal(STD_OUTPUT_HANDLE);
+  enable_virtual_terminal(STD_ERROR_HANDLE);
+}
+
+inline int wait_for_input(int timeout_ms){
+  HANDLE hIn = GetStdHandle(STD_INPUT_HANDLE);
+  DWORD rc = WaitForSingleObject(hIn, timeout_ms < 0 ? INFINITE : static_cast<DWORD>(timeout_ms));
+  if(rc == WAIT_TIMEOUT) return 0;
+  if(rc == WAIT_OBJECT_0) return 1;
+  return -1;
+}
+
+inline bool read_char(char &ch){
+  DWORD read = 0;
+  if(!ReadFile(GetStdHandle(STD_INPUT_HANDLE), &ch, 1, &read, nullptr)) return false;
+  return read == 1;
+}
+
+inline void write_stdout(const char* data, size_t len){
+  DWORD written = 0;
+  WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), data, static_cast<DWORD>(len), &written, nullptr);
+}
+
+inline void flush_stdout(){
+  _commit(_fileno(stdout));
+}
+
+inline bool env_var_exists(const std::string& key){
+  DWORD length = GetEnvironmentVariableA(key.c_str(), nullptr, 0);
+  if(length == 0){
+    DWORD err = GetLastError();
+    return err != ERROR_ENVVAR_NOT_FOUND;
+  }
+  return true;
+}
+
+inline void set_env(const std::string& key, const std::string& value, bool overwrite){
+  if(!overwrite && env_var_exists(key)) return;
+  _putenv_s(key.c_str(), value.c_str());
+}
+
+#else
+
+class TermRaw {
+  termios orig{};
+  bool active = false;
+public:
+  void enable(){
+    if(tcgetattr(STDIN_FILENO, &orig) == -1) std::exit(1);
+    termios raw = orig;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_cc[VMIN] = 1;
+    raw.c_cc[VTIME] = 0;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) std::exit(1);
+    active = true;
+  }
+  void disable(){
+    if(active){
+      tcsetattr(STDIN_FILENO, TCSANOW, &orig);
+      active = false;
+    }
+  }
+  ~TermRaw(){ disable(); }
+};
+
+inline void ensure_virtual_terminal_output(){}
+
+inline int wait_for_input(int timeout_ms){
+  struct pollfd pfd{STDIN_FILENO, POLLIN, 0};
+  int rc = ::poll(&pfd, 1, timeout_ms);
+  if(rc <= 0) return rc;
+  if(!(pfd.revents & POLLIN)) return 0;
+  return 1;
+}
+
+inline bool read_char(char &ch){
+  ssize_t n = ::read(STDIN_FILENO, &ch, 1);
+  return n == 1;
+}
+
+inline void write_stdout(const char* data, size_t len){
+  ::write(STDOUT_FILENO, data, len);
+}
+
+inline void flush_stdout(){
+  ::fsync(STDOUT_FILENO);
+}
+
+inline bool env_var_exists(const std::string& key){
+  return ::getenv(key.c_str()) != nullptr;
+}
+
+inline void set_env(const std::string& key, const std::string& value, bool overwrite){
+  ::setenv(key.c_str(), value.c_str(), overwrite ? 1 : 0);
+}
+
+#endif
+} // namespace platform
 
 // ===== Global state definitions =====
 ToolRegistry REG;
@@ -60,8 +215,8 @@ static void load_env_overrides(){
     std::string key = trim_copy(stripped.substr(0, eq));
     std::string value = trim_copy(stripped.substr(eq + 1));
     if(key.empty()) continue;
-    if(::getenv(key.c_str()) == nullptr){
-      ::setenv(key.c_str(), value.c_str(), 0);
+    if(!platform::env_var_exists(key)){
+      platform::set_env(key, value, false);
     }
   }
 }
@@ -432,7 +587,7 @@ bool set_config_home(const std::string& path, std::string& error){
   std::filesystem::path newPath = p;
   std::string newPathStr = newPath.string();
   if(oldPath == newPath){
-    ::setenv("HOME_PATH", newPathStr.c_str(), 1);
+    platform::set_env("HOME_PATH", newPathStr, true);
     persist_home_path_to_env(newPathStr);
     return true;
   }
@@ -465,7 +620,7 @@ bool set_config_home(const std::string& path, std::string& error){
     return false;
   }
   g_config_home = newPathStr;
-  ::setenv("HOME_PATH", newPathStr.c_str(), 1);
+  platform::set_env("HOME_PATH", newPathStr, true);
   persist_home_path_to_env(newPathStr);
   g_llm_watcher.initialized = false;
   g_llm_watcher.path.clear();
@@ -951,19 +1106,158 @@ void sortCandidatesByMatch(const std::string& query, Candidates& cand){
 // ===== Prompt params =====
 static const int extraLines = 3;
 
+struct CodepointRange {
+  char32_t first;
+  char32_t last;
+};
+
+static bool isInRange(char32_t cp, const CodepointRange& range){
+  return cp >= range.first && cp <= range.last;
+}
+
+static bool isCombining(char32_t cp){
+  // Combining mark ranges adapted from Markus Kuhn's wcwidth implementation
+  // (https://www.cl.cam.ac.uk/~mgk25/ucs/wcwidth.c) updated for modern Unicode.
+  static constexpr CodepointRange ranges[] = {
+    {0x0300, 0x036F}, {0x0483, 0x0489}, {0x0591, 0x05BD}, {0x05BF, 0x05BF},
+    {0x05C1, 0x05C2}, {0x05C4, 0x05C5}, {0x05C7, 0x05C7}, {0x0600, 0x0605},
+    {0x0610, 0x061A}, {0x061C, 0x061C}, {0x064B, 0x065F}, {0x0670, 0x0670},
+    {0x06D6, 0x06DD}, {0x06DF, 0x06E4}, {0x06E7, 0x06E8}, {0x06EA, 0x06ED},
+    {0x070F, 0x070F}, {0x0711, 0x0711}, {0x0730, 0x074A}, {0x07A6, 0x07B0},
+    {0x07EB, 0x07F3}, {0x07FD, 0x07FD}, {0x0816, 0x0819}, {0x081B, 0x0823},
+    {0x0825, 0x0827}, {0x0829, 0x082D}, {0x0859, 0x085B}, {0x08D3, 0x08E1},
+    {0x08E3, 0x0902}, {0x093A, 0x093A}, {0x093C, 0x093C}, {0x0941, 0x0948},
+    {0x094D, 0x094D}, {0x0951, 0x0957}, {0x0962, 0x0963}, {0x0981, 0x0981},
+    {0x09BC, 0x09BC}, {0x09C1, 0x09C4}, {0x09CD, 0x09CD}, {0x09E2, 0x09E3},
+    {0x09FE, 0x09FE}, {0x0A01, 0x0A02}, {0x0A3C, 0x0A3C}, {0x0A41, 0x0A42},
+    {0x0A47, 0x0A48}, {0x0A4B, 0x0A4D}, {0x0A51, 0x0A51}, {0x0A70, 0x0A71},
+    {0x0A75, 0x0A75}, {0x0A81, 0x0A82}, {0x0ABC, 0x0ABC}, {0x0AC1, 0x0AC5},
+    {0x0AC7, 0x0AC8}, {0x0ACD, 0x0ACD}, {0x0AE2, 0x0AE3}, {0x0AFA, 0x0AFF},
+    {0x0B01, 0x0B01}, {0x0B3C, 0x0B3C}, {0x0B3F, 0x0B3F}, {0x0B41, 0x0B44},
+    {0x0B4D, 0x0B4D}, {0x0B56, 0x0B56}, {0x0B62, 0x0B63}, {0x0B82, 0x0B82},
+    {0x0BC0, 0x0BC0}, {0x0BCD, 0x0BCD}, {0x0C00, 0x0C00}, {0x0C04, 0x0C04},
+    {0x0C3E, 0x0C40}, {0x0C46, 0x0C48}, {0x0C4A, 0x0C4D}, {0x0C55, 0x0C56},
+    {0x0C62, 0x0C63}, {0x0C81, 0x0C81}, {0x0CBC, 0x0CBC}, {0x0CBF, 0x0CBF},
+    {0x0CC6, 0x0CC6}, {0x0CCC, 0x0CCD}, {0x0CE2, 0x0CE3}, {0x0D00, 0x0D01},
+    {0x0D3B, 0x0D3C}, {0x0D41, 0x0D44}, {0x0D4D, 0x0D4D}, {0x0D62, 0x0D63},
+    {0x0D81, 0x0D81}, {0x0DCA, 0x0DCA}, {0x0DD2, 0x0DD4}, {0x0DD6, 0x0DD6},
+    {0x0E31, 0x0E31}, {0x0E34, 0x0E3A}, {0x0E47, 0x0E4E}, {0x0EB1, 0x0EB1},
+    {0x0EB4, 0x0EBC}, {0x0EC8, 0x0ECD}, {0x0F18, 0x0F19}, {0x0F35, 0x0F35},
+    {0x0F37, 0x0F37}, {0x0F39, 0x0F39}, {0x0F71, 0x0F7E}, {0x0F80, 0x0F84},
+    {0x0F86, 0x0F87}, {0x0F8D, 0x0F97}, {0x0F99, 0x0FBC}, {0x0FC6, 0x0FC6},
+    {0x102D, 0x1030}, {0x1032, 0x1037}, {0x1039, 0x103A}, {0x103D, 0x103E},
+    {0x1058, 0x1059}, {0x105E, 0x1060}, {0x1071, 0x1074}, {0x1082, 0x1082},
+    {0x1085, 0x1086}, {0x108D, 0x108D}, {0x109D, 0x109D}, {0x135D, 0x135F},
+    {0x1712, 0x1714}, {0x1732, 0x1734}, {0x1752, 0x1753}, {0x1772, 0x1773},
+    {0x17B4, 0x17B5}, {0x17B7, 0x17BD}, {0x17C6, 0x17C6}, {0x17C9, 0x17D3},
+    {0x17DD, 0x17DD}, {0x180B, 0x180D}, {0x180F, 0x180F}, {0x1885, 0x1886},
+    {0x18A9, 0x18A9}, {0x1920, 0x1922}, {0x1927, 0x1928}, {0x1932, 0x1932},
+    {0x1939, 0x193B}, {0x1A17, 0x1A18}, {0x1A1B, 0x1A1B}, {0x1A56, 0x1A56},
+    {0x1A58, 0x1A5E}, {0x1A60, 0x1A60}, {0x1A62, 0x1A62}, {0x1A65, 0x1A6C},
+    {0x1A73, 0x1A7C}, {0x1A7F, 0x1A7F}, {0x1AB0, 0x1ABE}, {0x1ABF, 0x1ACE},
+    {0x1B00, 0x1B03}, {0x1B34, 0x1B34}, {0x1B36, 0x1B3A}, {0x1B3C, 0x1B3C},
+    {0x1B42, 0x1B42}, {0x1B6B, 0x1B73}, {0x1B80, 0x1B81}, {0x1BA2, 0x1BA5},
+    {0x1BA8, 0x1BA9}, {0x1BAB, 0x1BAD}, {0x1BE6, 0x1BE6}, {0x1BE8, 0x1BE9},
+    {0x1BED, 0x1BED}, {0x1BEF, 0x1BF1}, {0x1C2C, 0x1C33}, {0x1C36, 0x1C37},
+    {0x1CD0, 0x1CD2}, {0x1CD4, 0x1CE0}, {0x1CE2, 0x1CE8}, {0x1CED, 0x1CED},
+    {0x1CF4, 0x1CF4}, {0x1CF8, 0x1CF9}, {0x1DC0, 0x1DF9}, {0x1DFB, 0x1DFF},
+    {0x200B, 0x200F}, {0x202A, 0x202E}, {0x2060, 0x2064}, {0x2066, 0x206F},
+    {0x20D0, 0x20DC}, {0x20DD, 0x20E0}, {0x20E1, 0x20E1}, {0x20E2, 0x20E4},
+    {0x20E5, 0x20F0}, {0x2CEF, 0x2CF1}, {0x2D7F, 0x2D7F}, {0x2DE0, 0x2DFF},
+    {0x302A, 0x302D}, {0x302E, 0x302F}, {0x3099, 0x309A}, {0xA66F, 0xA66F},
+    {0xA674, 0xA67D}, {0xA69E, 0xA69F}, {0xA6F0, 0xA6F1}, {0xA802, 0xA802},
+    {0xA806, 0xA806}, {0xA80B, 0xA80B}, {0xA825, 0xA826}, {0xA82C, 0xA82C},
+    {0xA8C4, 0xA8C5}, {0xA8E0, 0xA8F1}, {0xA8FF, 0xA8FF}, {0xA926, 0xA92D},
+    {0xA947, 0xA951}, {0xA980, 0xA982}, {0xA9B3, 0xA9B3}, {0xA9B6, 0xA9B9},
+    {0xA9BC, 0xA9BD}, {0xA9E5, 0xA9E5}, {0xAA29, 0xAA2E}, {0xAA31, 0xAA32},
+    {0xAA35, 0xAA36}, {0xAA43, 0xAA43}, {0xAA4C, 0xAA4C}, {0xAA7C, 0xAA7C},
+    {0xAAB0, 0xAAB0}, {0xAAB2, 0xAAB4}, {0xAAB7, 0xAAB8}, {0xAABE, 0xAABF},
+    {0xAAC1, 0xAAC1}, {0xAAEC, 0xAAED}, {0xAAF6, 0xAAF6}, {0xABE5, 0xABE5},
+    {0xABE8, 0xABE8}, {0xABED, 0xABED}, {0xFB1E, 0xFB1E}, {0xFE00, 0xFE0F},
+    {0xFE20, 0xFE2F}, {0x101FD, 0x101FD}, {0x102E0, 0x102E0},
+    {0x10376, 0x1037A}, {0x10A01, 0x10A03}, {0x10A05, 0x10A06},
+    {0x10A0C, 0x10A0F}, {0x10A38, 0x10A3A}, {0x10A3F, 0x10A3F},
+    {0x10AE5, 0x10AE6}, {0x10D24, 0x10D27}, {0x10EAB, 0x10EAC},
+    {0x10EFD, 0x10EFF}, {0x10F46, 0x10F50}, {0x10F82, 0x10F85},
+    {0x11001, 0x11001}, {0x11038, 0x11046}, {0x1107F, 0x11081},
+    {0x110B3, 0x110B6}, {0x110B9, 0x110BA}, {0x11100, 0x11102},
+    {0x11127, 0x1112B}, {0x1112D, 0x11134}, {0x11173, 0x11173},
+    {0x11180, 0x11181}, {0x111B6, 0x111BE}, {0x111C9, 0x111CC},
+    {0x111CF, 0x111CF}, {0x1122F, 0x11231}, {0x11234, 0x11234},
+    {0x11236, 0x11237}, {0x1123E, 0x1123E}, {0x112DF, 0x112DF},
+    {0x112E3, 0x112EA}, {0x11300, 0x11301}, {0x1133B, 0x1133C},
+    {0x11340, 0x11340}, {0x11366, 0x1136C}, {0x11370, 0x11374},
+    {0x11438, 0x1143F}, {0x11442, 0x11444}, {0x11446, 0x11446},
+    {0x1145E, 0x1145E}, {0x114B3, 0x114B8}, {0x114BA, 0x114BA},
+    {0x114BF, 0x114C0}, {0x114C2, 0x114C3}, {0x115B2, 0x115B5},
+    {0x115BC, 0x115BD}, {0x115BF, 0x115C0}, {0x115DC, 0x115DD},
+    {0x11633, 0x1163A}, {0x1163D, 0x1163D}, {0x1163F, 0x11640},
+    {0x116AB, 0x116AB}, {0x116AD, 0x116AD}, {0x116B0, 0x116B5},
+    {0x116B7, 0x116B7}, {0x1171D, 0x1171F}, {0x11722, 0x11725},
+    {0x11727, 0x1172B}, {0x1182F, 0x11837}, {0x11839, 0x1183A},
+    {0x1193B, 0x1193C}, {0x1193E, 0x1193E}, {0x11943, 0x11943},
+    {0x119D4, 0x119D7}, {0x119DA, 0x119DB}, {0x119E0, 0x119E0},
+    {0x11A01, 0x11A0A}, {0x11A33, 0x11A38}, {0x11A3B, 0x11A3E},
+    {0x11A47, 0x11A47}, {0x11A51, 0x11A56}, {0x11A59, 0x11A5B},
+    {0x11A8A, 0x11A96}, {0x11A98, 0x11A99}, {0x11C30, 0x11C36},
+    {0x11C38, 0x11C3D}, {0x11C3F, 0x11C3F}, {0x11C92, 0x11CA7},
+    {0x11CAA, 0x11CB0}, {0x11CB2, 0x11CB3}, {0x11CB5, 0x11CB6},
+    {0x11D31, 0x11D36}, {0x11D3A, 0x11D3A}, {0x11D3C, 0x11D3D},
+    {0x11D3F, 0x11D45}, {0x11D47, 0x11D47}, {0x11D90, 0x11D91},
+    {0x11D95, 0x11D95}, {0x11D97, 0x11D97}, {0x11EF3, 0x11EF4},
+    {0x13430, 0x13438}, {0x16AF0, 0x16AF4}, {0x16B30, 0x16B36},
+    {0x16F4F, 0x16F4F}, {0x16F8F, 0x16F92}, {0x16FE4, 0x16FE4},
+    {0x16FF0, 0x16FF1}, {0x1BC9D, 0x1BC9E}, {0x1BCA0, 0x1BCA3},
+    {0x1D167, 0x1D169}, {0x1D173, 0x1D182}, {0x1D185, 0x1D18B},
+    {0x1D1AA, 0x1D1AD}, {0x1D242, 0x1D244}, {0x1DA00, 0x1DA36},
+    {0x1DA3B, 0x1DA6C}, {0x1DA75, 0x1DA75}, {0x1DA84, 0x1DA84},
+    {0x1DA9B, 0x1DA9F}, {0x1DAA1, 0x1DAAF}, {0x1E000, 0x1E006},
+    {0x1E008, 0x1E018}, {0x1E01B, 0x1E021}, {0x1E023, 0x1E024},
+    {0x1E026, 0x1E02A}, {0x1E08F, 0x1E08F}, {0x1E130, 0x1E136},
+    {0x1E2AE, 0x1E2AE}, {0x1E2EC, 0x1E2EF}, {0x1E4EC, 0x1E4EF},
+    {0x1E8D0, 0x1E8D6}, {0x1E944, 0x1E94A}, {0xE0100, 0xE01EF}
+  };
+  for(const auto& range : ranges){
+    if(cp < range.first) break;
+    if(isInRange(cp, range)) return true;
+  }
+  return false;
+}
+
+static int codepointWidth(char32_t cp){
+  if(cp == 0) return 0;
+  if(cp < 0x20 || (cp >= 0x7F && cp < 0xA0)) return 0;
+  if(isCombining(cp)) return 0;
+  if(cp >= 0x1100 &&
+     (cp <= 0x115F ||
+      cp == 0x2329 || cp == 0x232A ||
+      (cp >= 0x2E80 && cp <= 0xA4CF && cp != 0x303F) ||
+      (cp >= 0xAC00 && cp <= 0xD7A3) ||
+      (cp >= 0xF900 && cp <= 0xFAFF) ||
+      (cp >= 0xFE10 && cp <= 0xFE19) ||
+      (cp >= 0xFE30 && cp <= 0xFE6F) ||
+      (cp >= 0xFF00 && cp <= 0xFF60) ||
+      (cp >= 0xFFE0 && cp <= 0xFFE6) ||
+      (cp >= 0x1F300 && cp <= 0x1F64F) ||
+      (cp >= 0x1F680 && cp <= 0x1F6FF) ||
+      (cp >= 0x1F900 && cp <= 0x1FAFF) ||
+      (cp >= 0x20000 && cp <= 0x2FFFD) ||
+      (cp >= 0x30000 && cp <= 0x3FFFD)))
+    return 2;
+  return 1;
+}
+
 static int displayWidth(const std::string& text){
-  static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conv;
-  std::wstring ws;
+  static std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+  std::u32string codepoints;
   try {
-    ws = conv.from_bytes(text);
+    codepoints = conv.from_bytes(text);
   } catch (const std::range_error&) {
     return static_cast<int>(text.size());
   }
   int width = 0;
-  for (wchar_t ch : ws) {
-    int w = ::wcwidth(ch);
-    if (w < 0) w = 1;
-    width += w;
+  for(char32_t cp : codepoints){
+    width += codepointWidth(cp);
   }
   return width;
 }
@@ -1673,20 +1967,23 @@ int main(){
   register_tools_from_config(conf);
 
   // 3) 退出时回车复位
-  std::atexit([](){ ::write(STDOUT_FILENO, "\r\n", 2); ::fsync(STDOUT_FILENO); });
-  std::signal(SIGINT,  [](int){ ::write(STDOUT_FILENO, "\r\n", 2); ::_exit(128); });
-  std::signal(SIGTERM, [](int){ ::write(STDOUT_FILENO, "\r\n", 2); ::_exit(128); });
-  std::signal(SIGHUP,  [](int){ ::write(STDOUT_FILENO, "\r\n", 2); ::_exit(128); });
-  std::signal(SIGQUIT, [](int){ ::write(STDOUT_FILENO, "\r\n", 2); ::_exit(128); });
+  std::atexit([](){ platform::write_stdout("\r\n", 2); platform::flush_stdout(); });
+#ifdef SIGINT
+  std::signal(SIGINT,  [](int){ platform::write_stdout("\r\n", 2); std::_Exit(128); });
+#endif
+#ifdef SIGTERM
+  std::signal(SIGTERM, [](int){ platform::write_stdout("\r\n", 2); std::_Exit(128); });
+#endif
+#ifdef SIGHUP
+  std::signal(SIGHUP,  [](int){ platform::write_stdout("\r\n", 2); std::_Exit(128); });
+#endif
+#ifdef SIGQUIT
+  std::signal(SIGQUIT, [](int){ platform::write_stdout("\r\n", 2); std::_Exit(128); });
+#endif
 
   // 4) 原始模式（最小化）
-  struct TermRaw { termios orig{}; bool active=false;
-    void enable(){ if(tcgetattr(STDIN_FILENO,&orig)==-1) std::exit(1);
-      termios raw=orig; raw.c_lflag&=~(ECHO|ICANON); raw.c_cc[VMIN]=1; raw.c_cc[VTIME]=0;
-      if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)==-1) std::exit(1); active=true; }
-    void disable(){ if(active){ tcsetattr(STDIN_FILENO,TCSANOW,&orig); active=false; } }
-    ~TermRaw(){ disable(); }
-  } term; term.enable();
+  platform::ensure_virtual_terminal_output();
+  platform::TermRaw term; term.enable();
 
   std::string buf;
   int sel = 0;
@@ -1777,8 +2074,7 @@ int main(){
       renderFrame();
     }
 
-    struct pollfd pfd{STDIN_FILENO, POLLIN, 0};
-    int rc = ::poll(&pfd, 1, 200);
+    int rc = platform::wait_for_input(200);
     if(rc == 0){
       bool beforeMsg = lastMessageUnread;
       bool beforeLlm = lastLlmUnread;
@@ -1794,15 +2090,13 @@ int main(){
       continue;
     }
     if(rc < 0){
+#ifndef _WIN32
       if(errno == EINTR) continue;
+#endif
       break;
     }
-    if(!(pfd.revents & POLLIN)){
-      continue;
-    }
     char ch;
-    ssize_t n = ::read(STDIN_FILENO, &ch, 1);
-    if(n <= 0) break;
+    if(!platform::read_char(ch)) break;
 
     if(ch=='\n' || ch=='\r'){
       std::cout << "\n";
@@ -1840,8 +2134,8 @@ int main(){
     }
     if(ch=='\x1b'){
       char seq[2];
-      if(::read(STDIN_FILENO,&seq[0],1)<=0) continue;
-      if(::read(STDIN_FILENO,&seq[1],1)<=0) continue;
+      if(!platform::read_char(seq[0])) continue;
+      if(!platform::read_char(seq[1])) continue;
       if(seq[0]=='['){
         if(seq[1]=='A'){
           if(haveCand && total>0){
@@ -1865,6 +2159,6 @@ int main(){
     }
   }
 
-  ::write(STDOUT_FILENO, "\r\n", 2); ::fsync(STDOUT_FILENO);
+  platform::write_stdout("\r\n", 2); platform::flush_stdout();
   return 0;
 }
